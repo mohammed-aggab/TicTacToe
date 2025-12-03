8048219bec5d84159884d10744272dcba454ddb2
mohammed-aggab <mohammed.aggab@laplateforme.io>
2025-12-03 15:49:21 +0100

    ajout du fichier mohammed_aggab.py

---
+++ mohammed_aggab.py
@@ -0,0 +1,16 @@
+prenom = input("entrer votre nom")
+print(prenom)
+nombre = int(input("entrer un nombre entre 1 et 10"))
+print(nombre)
+while not 1<nombre <10:
+    nombre = int(input("entrer le bon nombre"))
+    
+    
+def saluer(nom):
+    print("bienvenue",nom)
+print(saluer("mohammed"))
+
+nombre = int(input("entrer nombre"))
+
+for i in range(nombre):
+    print("repetition X :python c 'est coul")
